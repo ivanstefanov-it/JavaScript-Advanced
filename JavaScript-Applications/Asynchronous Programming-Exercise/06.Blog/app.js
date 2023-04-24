d630e197a5e8a9fb66d38367c2a03149fac4de9c
ivanstefanov-it <ivanstefanov.it@gmail.com>
2023-04-24 10:01:14 +0300

    Added Asynchronous Programming

---
+++ JavaScript-Applications/Asynchronous Programming-Exercise/06.Blog/app.js
@@ -0,0 +1,53 @@
+function attachEvents() {
+    document.getElementById('btnLoadPosts').addEventListener('click', getPosts);
+    document.getElementById('btnViewPost').addEventListener('click', displayPost);
+}
+attachEvents();
+
+async function getPosts(){
+    const url = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const respons = await fetch(url);
+    const data = await respons.json();
+    
+    const select = document.getElementById('posts');
+
+    Object.values(data).map(createOption).forEach(o => select.appendChild(o));
+}
+
+function createOption(post){
+    const result = document.createElement('option');
+    result.textContent = post.title;
+    result.value = post.id;
+
+    return result;
+}
+
+function displayPost(){
+    const postId = document.getElementById('posts').value;
+    getCommentsByPostId(postId);
+}
+
+async function getCommentsByPostId(postId){
+    const postRespons = await fetch('http://localhost:3030/jsonstore/blog/posts/' + postId);
+    const postData = await postRespons.json();
+
+    document.getElementById('post-title').textContent = postData.title;
+    document.getElementById('post-body').textContent = postData.body;
+
+    const url = 'http://localhost:3030/jsonstore/blog/comments';
+
+    const commentRespons = await fetch(url);
+    const commentData = await commentRespons.json();
+    const comments = Object.values(commentData).filter(c => c.postId == postId)
+
+    const commentsUl = document.getElementById('post-comments');
+    comments.map(createComment).forEach(c => commentsUl.appendChild(c));
+}
+
+function createComment(comment){
+    const result = document.createElement('li');
+    result.textContent = comment.text;
+    result.id = comment.id;
+    return result;
+}
