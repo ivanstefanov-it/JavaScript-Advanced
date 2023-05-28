9db5869333894ff1660cf112aff4ff5127a87579
ivanstefanov-it <ivanstefanov.it@gmail.com>
2023-05-28 20:08:45 +0300

    Add Arithmetic expression exercise

---
+++ JavaScript Tutorial For Beginner/Arithmetic expression/index.js
@@ -0,0 +1,15 @@
+let students = 20;
+students = students + 1; // students += 1;
+console.log(students);
+
+students = students - 1; // students -= 1;
+console.log(students);
+
+students = students * 2; // students *= 2;
+console.log(students);
+
+students = students / 10; // students /= 10;
+console.log(students);
+
+let extraStudents = students % 3;
+console.log(extraStudents);
