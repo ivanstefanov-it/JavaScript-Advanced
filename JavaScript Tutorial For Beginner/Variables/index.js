8e5db027abc6ca8747e99a7de6dd1bba93395296
ivanstefanov-it <ivanstefanov.it@gmail.com>
2023-05-28 20:00:24 +0300

    Add Variables exercise

---
+++ JavaScript Tutorial For Beginner/Variables/index.js
@@ -0,0 +1,13 @@
+let firstName = "Ivan";
+let age;
+age = 30;
+age++;
+let student = true;
+
+console.log("Hello",firstName);
+console.log("You are",age, "years old!");
+console.log("Enrolled", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old!";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
