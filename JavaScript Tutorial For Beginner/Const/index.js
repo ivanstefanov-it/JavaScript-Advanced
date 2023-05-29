3cfa2f5892b763945c31705326300345d30123d6
ivanstefanov-it <ivanstefanov.it@gmail.com>
2023-05-29 10:41:39 +0300

    Add Const exercise

---
+++ JavaScript Tutorial For Beginner/Const/index.js
@@ -0,0 +1,10 @@
+const pi = 3.14159;
+let radius;
+let curcumference;
+
+radius = window.prompt("Enter a radius of a circle");
+radius = Number(radius);
+
+curcumference = 2 * pi * radius;
+
+console.log(curcumference);
