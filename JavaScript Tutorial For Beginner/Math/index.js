e6c3ee91b434717b423c92567f8352b79ecaaeaf
ivanstefanov-it <ivanstefanov.it@gmail.com>
2023-05-29 10:50:55 +0300

    Add Math exercise

---
+++ JavaScript Tutorial For Beginner/Math/index.js
@@ -0,0 +1,29 @@
+let x = 3.14;
+let y = 5;
+let z = 9;
+let max;
+let min;
+
+//x = Math.round(x);
+//console.log(x) // --> 3
+
+//x = Math.floor(x);
+//console.log(x) // --> 3
+
+//x = Math.ceil(x);
+//console.log(x) // --> 4
+
+//x = Math.pow(x, 2);
+//console.log(x) // --> 9.8596
+
+//x = Math.sqrt(x);
+//console.log(x) // --> 1.7720
+
+//x = Math.abs(x);
+//console.log(x) // --> 9.8596
+
+max = Math.max(x, y, z);
+console.log(max);
+
+min = Math.min(x, y, z);
+console.log(min);
