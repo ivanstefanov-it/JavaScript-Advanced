4feece25d2034143ca2d084277f8f3ed97cc031b
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-05 10:43:35 +0300

    Add Useful String Methods exercise

---
+++ JavaScript Tutorial For Beginner/Useful String Methods/index.js
@@ -0,0 +1,26 @@
+let username = "Ivan Stefanov";
+let phoneNumber = "123-456-7890";
+
+let nameLength = username.length;
+console.log(nameLength);
+
+let firstLetter = username.charAt(0);
+console.log(firstLetter);
+
+let index = username.indexOf("S");
+console.log(index);
+
+let lastIndex = username.lastIndexOf("a");
+console.log(lastIndex);
+
+let trimmed = username.trim();
+console.log(trimmed);
+
+let upperCase = username.toUpperCase();
+console.log(upperCase);
+
+let lowerCase = username.toLocaleLowerCase();
+console.log(lowerCase);
+
+let newPhoneNumber = phoneNumber.replaceAll("-", "/");
+console.log(newPhoneNumber);
