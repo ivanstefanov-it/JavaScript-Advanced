ac4c768cda9eaa2ab688d5f5a80fb23db4c68c21
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-05 11:10:24 +0300

    Add If Statements exercise

---
+++ JavaScript Tutorial For Beginner/If Statements/index.js
@@ -0,0 +1,24 @@
+let age = 70;
+
+if(age >= 65){
+    console.log("You are a senior citizen!");
+}
+else if (age >= 18) {
+    console.log("You are an adult!");
+} 
+else if(age < 0){
+    console.log("You haven't been born yet!");
+}
+else {
+    console.log("You are a child!");
+}
+
+
+let online = true;
+
+if (online) {
+    console.log("You are online!")
+} 
+else {
+    console.log("You are offline!")
+}
