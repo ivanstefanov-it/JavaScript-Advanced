244e0a3998d1377dc05faa228f5c32d4b255c56b
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-05 11:47:03 +0300

    Add Switches exercise

---
+++ JavaScript Tutorial For Beginner/Switches/index.js
@@ -0,0 +1,43 @@
+let grade = "A";
+
+switch (grade) {
+    case "A":
+        console.log("You did great!");
+        break;
+    case "B":
+        console.log("You did good!");
+        break;
+    case "C":
+        console.log("You did okay!");
+        break;
+    case "D":
+        console.log("You passes ... barely!");
+        break;
+    case "F":
+        console.log("You FAILED!");
+        break;
+    default:
+        console.log(grade, "is not a letter grade!");
+}
+
+let numberGrade = 55;
+
+switch (true) {
+    case numberGrade >= 90:
+        console.log("You did great!");
+        break;
+    case numberGrade >= 80:
+        console.log("You did good!");
+        break;
+    case numberGrade >= 70:
+        console.log("You did okay!");
+        break;
+    case numberGrade >= 60:
+        console.log("You passes ... barely!");
+        break;
+    case numberGrade < 60:
+        console.log("You FAILED!");
+        break;
+    default:
+        console.log(grade, "is not a letter grade!");
+}
