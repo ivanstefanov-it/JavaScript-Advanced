74e9ccfbea2130a13099875165919dff94be6384
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-05 10:55:16 +0300

    Add Method Chaining exercise

---
+++ JavaScript Tutorial For Beginner/Method Chaining/index.js
@@ -0,0 +1,8 @@
+let username = "ivan";
+
+//let firstLetter = username.charAt(0);
+//firstLetter = firstLetter.toUpperCase();
+//console.log(firstLetter);
+
+let firstLetter = username.charAt(0).toUpperCase().trim();
+console.log(firstLetter);
