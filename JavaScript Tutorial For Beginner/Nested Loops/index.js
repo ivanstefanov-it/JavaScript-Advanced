97d55ceb8b3479b0f2a4382f717ffe020126adc4
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-07 10:12:37 +0300

    Add Nested Loops exercise

---
+++ JavaScript Tutorial For Beginner/Nested Loops/index.js
@@ -0,0 +1,10 @@
+let symbol = window.prompt("Enter a symbol to use");
+let rows = window.prompt("Enter # of rows");
+let cols = window.prompt("Enter # of columns");
+
+for (let i = 1; i <= rows; i++) {
+   for (let j = 1; j <= cols; j++) {
+      document.getElementById("myRectangle").innerHTML += symbol;
+   }
+   document.getElementById("myRectangle").innerHTML += "<br>";
+}
