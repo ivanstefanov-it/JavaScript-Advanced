6bf03709608902912a7dc1dd4fd00c8b8bacb290
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-07 09:42:26 +0300

    Add NOT logical operator exercise

---
+++ JavaScript Tutorial For Beginner/NOT logical operator/index.js
@@ -0,0 +1,16 @@
+let temp = 15;
+let sunny = true;
+
+if (!(temp > 0)) {
+    console.log("It is cold outside");
+}
+else{
+    console.log("It is warm outside");
+}
+
+if (!sunny) {
+    console.log("It is cloudy outside");
+}
+else{
+    console.log("It is sunny outside");
+}
