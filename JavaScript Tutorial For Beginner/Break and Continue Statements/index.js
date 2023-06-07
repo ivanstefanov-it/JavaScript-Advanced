f4a98c60f6769d7d42fa152c8eb29703045bab88
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-07 10:05:06 +0300

    Add Break and Continue Statements exercise

---
+++ JavaScript Tutorial For Beginner/Break and Continue Statements/index.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 20; i++) {
+   if (i == 13) {
+    break;
+   }
+   console.log(i);
+}
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 13) {
+     continue;
+    }
+    console.log(i);
+ }
