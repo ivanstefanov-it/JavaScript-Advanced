7b0a715313978fc062f3de389c9b829f499864fd
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-06-07 10:21:05 +0300

    Add Functions exercise

---
+++ JavaScript Tutorial For Beginner/Functions/index.js
@@ -0,0 +1,11 @@
+let userName = "Ivan"
+let age = 30;
+happyBirthday(userName, age);
+
+function happyBirthday(userName, age){
+   console.log("Happy birthday to you!");
+   console.log("Happy birthday to you!");
+   console.log("Happy birthday dear", userName);
+   console.log("Happy birthday to you!");
+   console.log("You are", age,"years old!");
+}
