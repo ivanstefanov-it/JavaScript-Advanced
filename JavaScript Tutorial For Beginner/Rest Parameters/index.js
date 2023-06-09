743706195589f4ae03ed551c99cfefe2c421ccb8
ivan.stefanov <ivanstefanov.it@gmail.com>
2023-06-09 12:00:25 +0300

    Add Rest Parameters exercise

---
+++ JavaScript Tutorial For Beginner/Rest Parameters/index.js
@@ -0,0 +1,42 @@
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+/*
+console.log(sum(a, b));
+
+function sum(a, b){
+    return a + b;
+}
+*/
+
+/*
+console.log(sum(a, b, c));
+
+function sum(a, b, c){
+    return a + b + c;
+}
+*/
+
+/*
+console.log(sum(a, b, c, d));
+
+function sum(a, b, c, d){
+    return a + b + c + d;
+}
+*/
+
+function sum(...numbers){
+    let total = 0;
+    for(let number of numbers){
+        total += number;
+    }
+    return total;
+}
+
+console.log(sum(a, b));//3
+console.log(sum(a, b, c));//6
+console.log(sum(a, b, c, d));//10
+console.log(sum(a, b, c, d, e));//15
