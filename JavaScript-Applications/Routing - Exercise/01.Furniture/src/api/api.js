9423d2a913ed604ba3c3afe68adafea6496c817f
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-09-13 17:16:25 +0300

    Add details, edit, create and all furniture pages

--- JavaScript-Applications/Routing - Exercise/01.Furniture/src/api/api.js
+++ JavaScript-Applications/Routing - Exercise/01.Furniture/src/api/api.js
@@ -2,30 +2,34 @@ export const settings = {
     host: ''
 };
 
-async function request(url, options){
-    try{
-    const response = await fetch(url, options);
+async function request(url, options) {
+    try {
+        const response = await fetch(url, options);
 
-    if (response.ok == false) {
-        const error = await response.json();
-        throw new Error(error.message);
-    }
+        if (response.ok == false) {
+            if (response.status == 401) {
+                sessionStorage.removeItem('email');
+                sessionStorage.removeItem('authToken');
+                sessionStorage.removeItem('userId');
+            }
+            const error = await response.json();
+            throw new Error(error.message);
+        }
 
-    try{
-        const data = await response.json();
-        return data;
-    } catch(err){
-        return response;
-    }
-
-    } catch(err){
+        try {
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            return response;
+        }
+    } catch (err) {
         alert(err.message);
         throw err;
     }
 }
 
-function getOptions(method = 'get', body){
-    const options ={
+function getOptions(method = 'get', body) {
+    const options = {
         method,
         headers: {}
     };
@@ -43,42 +47,48 @@ function getOptions(method = 'get', body){
     return options;
 }
 
-export async function get(url){
+export async function get(url) {
     return await request(url, getOptions());
 }
 
-export async function post(url, data){
+export async function post(url, data) {
     return await request(url, getOptions('post', data));
 }
 
-export async function put(url, data){
+export async function put(url, data) {
     return await request(url, getOptions('put', data));
 }
 
-export async function del(url){
+export async function del(url) {
     return await request(url, getOptions('delete'));
 }
 
-export async function login(email, password){
+export async function login(email, password) {
     const result = await post(settings.host + '/users/login', { email, password });
+
     sessionStorage.setItem('email', result.email);
     sessionStorage.setItem('authToken', result.accessToken);
     sessionStorage.setItem('userId', result._id);
+
     return result;
 }
 
-export async function register(email, password){
+export async function register(email, password) {
     const result = await post(settings.host + '/users/register', { email, password });
+
     sessionStorage.setItem('email', result.email);
     sessionStorage.setItem('authToken', result.accessToken);
     sessionStorage.setItem('userId', result._id);
+
     return result;
 }
 
-export async function logout(){
+export async function logout() {
     const result = await get(settings.host + '/users/logout');
+
     sessionStorage.removeItem('email');
     sessionStorage.removeItem('authToken');
     sessionStorage.removeItem('userId');
+
     return result;
 }
--- JavaScript-Applications/Routing - Exercise/01.Furniture/src/views/dashboard.js
+++ JavaScript-Applications/Routing - Exercise/01.Furniture/src/views/dashboard.js
@@ -1,5 +1,6 @@
 import {html} from '../../node_modules/lit-html/lit-html.js';
 import { getFurniture } from '../api/data.js';
+import { itemTemplate } from './common/item.js';
 
 const dashboardTemplate = (data) => html` 
 <div class="row space-top">
@@ -10,22 +11,6 @@ const dashboardTemplate = (data) => html`
 </div>
 <div class="row space-top">
     ${data.map(itemTemplate)}
-</div>`;
-
-const itemTemplate = (item) => html`
-<div class="col-md-4">
-<div class="card text-white bg-primary">
-    <div class="card-body">
-            <img src=${item.img} />
-            <p>${item.description}<p>
-            <footer>
-                <p>Price: <span>${item.price} $</span></p>
-            </footer>
-            <div>
-                <a href=${`/details/${item._id}`} class="btn btn-info">Details</a>
-            </div>
-    </div>
-</div>
 </div>`; 
 
 export async function dashboardPage(ctx){
